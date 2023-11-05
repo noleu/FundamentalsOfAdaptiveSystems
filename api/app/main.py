d46dbe916826184ada5222ac6992f662b4ff3ded
Tiels Thiele <noleu66@posteo.net>
2023-11-05 20:43:08 +0100

    added endpoint

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-from .routers import example
+from .routers import example, execute_schema
 
 app = FastAPI()
 
 app.include_router(example.router, prefix="/example")
+app.include_router(execute_schema.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!----"}
