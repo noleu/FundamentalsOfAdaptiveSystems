65b0f91c05a0ad31fc41685892d5a71a89b29cce
Jeremy Diamond <jeremygdiamond@gmail.com>
2023-11-15 14:44:45 +0100

    added bounds from lecture to ad_op

--- api/app/routers/adaptation_options.py
+++ api/app/routers/adaptation_options.py
@@ -13,13 +13,13 @@ router = APIRouter(
 def adaptation_options_endpoint():
 
     return {
-        "route_random_sigma":           {"start": 0.0,"stop": 1.0,"domain": "continuous"} , 
-                        "exploration_percentage":       {"start": 0.0,"stop": 1.0,"domain": "continuous"}, 
-                        "max_speed_and_length_factor":  {"start": 0.0,"stop": 2**31-1,"domain": "continuous"},
-                        "average_edge_duration_factor": {"start": 0.0,"stop": 2**31-1,"domain": "continuous"}, 
-                        "freshness_update_factor":      {"start": 1.0,"stop": 2**31-1,"domain": "continuous"}, 
-                        "freshness_cut_off_value":      {"start": 1.0,"stop": 2**31-1,"domain": "continuous"},
-                        "re_route_every_ticks":         {"start": 1,"stop": 2**31-1,"domain": "discrete"}, 
-                        "total_car_counter":            {"start": 10,"stop": 2**31-1,"domain": "discrete"}, 
-                        "edge_average_influence":       {"start": 0.0,"stop": 1.0,"domain": "continuous"}
-                        }
+            "route_random_sigma":           {"start": 0.0,"stop": 0.3,"domain": "continuous"} , 
+            "exploration_percentage":       {"start": 0.0,"stop": 0.3,"domain": "continuous"}, 
+            "max_speed_and_length_factor":  {"start": 1,"stop": 2.5,"domain": "continuous"},
+            "average_edge_duration_factor": {"start": 1,"stop": 2.5,"domain": "continuous"}, 
+            "freshness_update_factor":      {"start": 5,"stop": 20,"domain": "continuous"}, 
+            "freshness_cut_off_value":      {"start": 100,"stop": 700,"domain": "discrete"},
+            "re_route_every_ticks":         {"start": 10,"stop": 70,"domain": "discrete"}, 
+            "total_car_counter":            {"start": 10,"stop": 2**31-1,"domain": "discrete"}, 
+            "edge_average_influence":       {"start": 0.0,"stop": 1.0,"domain": "continuous"}
+            }
