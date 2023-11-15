9b2c6d329d91781f049a06c567f836491439840e
Qi-Mo Lin <qimolin9@gmail.com>
2023-11-15 13:14:47 +0100

    remove deprecated startup and unused packages

--- api/app/main.py
+++ api/app/main.py
@@ -2,9 +2,19 @@ from fastapi import FastAPI
 from .routers import adaptation_options, adaptation_options_schema, execute_schema, monitor_schema, monitor, execute
 from .connectors import KafkaConsumerMonitor
 from threading import Thread
-import asyncio
-from aiokafka import AIOKafkaProducer
-app = FastAPI()
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def startup(app: FastAPI):
+    consumerThread = Thread(target=KafkaConsumerMonitor.connect)
+    consumerThread.start()
+    yield
+
+app = FastAPI(lifespan=startup)
+    
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!----"}
 
 app.include_router(monitor.router)
 app.include_router(monitor_schema.router)
@@ -12,13 +22,3 @@ app.include_router(execute.router)
 app.include_router(execute_schema.router)
 app.include_router(adaptation_options.router)
 app.include_router(adaptation_options_schema.router)
-
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!----"}
-
-@app.on_event("startup")
-async def startup_event():
-    await asyncio.sleep(35)
-    consumerThread = Thread(target=KafkaConsumerMonitor.connect)
-    consumerThread.start()
